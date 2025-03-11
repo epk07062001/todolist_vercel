13838b38ca211c12a16dcd7f0c455d4c70c6a365
praveen <96836460+epk07062001@users.noreply.github.com>
2025-03-11 12:58:14 +0530

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import TodoList from "@/components/todo-list"
+
+export default function Home() {
+  return (
+    <div className="container mx-auto py-10 px-4 max-w-2xl">
+      <TodoList />
+    </div>
+  )
+}
+
